ac26e28256339fbe391a5fb0c02073c233b5eff6
Yannick_janssen <yannick_janssen@tud11687.tudelft.net>
2016-05-10 16:20:09 +0200

    random action was not saved

--- khepera/random_action.cpp
+++ khepera/random_action.cpp
@@ -5,7 +5,7 @@
 int Random_action::chooseAction(blackboard *BLKB)
 {
     action = static_cast<Action_heading> ( rand()%3 );
-    BLKB->set("action",action);
+    BLKB->set("action",action   );
 
     return action;
 }
