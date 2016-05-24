ca4ec10ae27ffa96d575ba470fdaf6cb3817692a
Yannick_janssen <yannick_janssen@tud11687.tudelft.net>
2016-05-24 13:29:27 +0200

    Additional behavior tree structure for genetic programming

---
+++ khepera/bt_test_gp_kirk.cpp
@@ -0,0 +1,6 @@
+#include "bt_test_gp_kirk.h"
+
+bt_test_gp_kirk::bt_test_gp_kirk()
+{
+
+}
---
+++ khepera/bt_test_gp_kirk.h
@@ -0,0 +1,14 @@
+#ifndef BT_TEST_GP_KIRK_H
+#define BT_TEST_GP_KIRK_H
+
+#include "../BT/behaviour.h"
+#include "../BT_leafs/kheperaActions.h"
+#include "../BT_leafs/conditions.h"
+
+class bt_test_gp_kirk
+{
+public:
+    bt_test_gp_kirk();
+};
+
+#endif // BT_TEST_GP_KIRK_H
